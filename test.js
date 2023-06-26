2f91704edcd0f66edbaaa1a8fc5a73cfcad09d6b
whoyou <61051032+idontseven@users.noreply.github.com>
2023-06-26 20:26:54 +0300

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-let btn = document.querySelector('#start_test')
-btn.addEventListener('click',function(){
-    window.location.href = 'start_test.html'
-})
-let slova = ['']
-function test(){
-    pass
-}
